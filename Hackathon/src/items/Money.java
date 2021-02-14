f07162635e17aa424e177d5d6f0091e89ce98992
Rafael Flores Souza <Raflso@icloud.com>
2021-02-13 22:47:30 -0400

    Refactored Money Class

--- Hackathon/src/items/Money.java
+++ Hackathon/src/items/Money.java
@@ -1,21 +1,22 @@
 package items;
 import javafx.scene.image.Image;
+import tools.Load;
 
-public class Money {
-
-	// Bill Types
-	public static enum Bill_Type {FIVE, TWENTY, HUNDRED};
+public enum Money {
+	FIVE(5, Load.newImage("objects/money5.png")),
+	TWENTY(20, Load.newImage("objects/money20.png")),
+	HUNDRED(100, Load.newImage("objects/money100.png"));
 
 	//value of the money
 	private int value;
 	public int value() {return value;}
-	
+
 	//image of the money
 	private Image image;
 	public Image image() {return image;}
-	
+
 	//constructor for money
-	public Money (int value, Image image) {
+	private Money(int value, Image image) {
 		this.value = value;
 		this.image = image;
 	}
