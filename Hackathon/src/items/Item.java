4a96b60cec46e06783ac987f8e2297c2ddb87bda
alexs2112 <alexander.stevenson@ucalgary.ca>
2021-02-14 02:51:07 -0700

    Added remaining dollar bills

--- Hackathon/src/items/Item.java
+++ Hackathon/src/items/Item.java
@@ -6,7 +6,9 @@ import tools.Load;
 public enum Item {
 
     FIVE("$5", 5, 0, Load.newImage("objects/money5.png")),
+    TEN("$10", 10, 0, Load.newImage("objects/money10.png")),
     TWENTY("$20", 20, 0, Load.newImage("objects/money20.png")),
+    FIFTY("$50", 50, 0, Load.newImage("objects/money50.png")),
     HUNDRED("$100", 100, 0, Load.newImage("objects/money100.png")),
     BACON("Bacon", 0, 3, Load.newImage("objects/bacon.png")),
     POUTINE("Poutine", 0, 5,Load.newImage("objects/poutine.png")),
@@ -42,12 +44,16 @@ public enum Item {
      * Returns a random bill type
      */
     public static Item getRandomBillType(){
-        int randNum = (int)(Math.random() * 3);
+        int randNum = (int)(Math.random() * 5);
 
         if (randNum == 0){
             return FIVE;
         } else if (randNum == 1){
-            return TWENTY;
+            return TEN;
+        } else if (randNum == 2) {
+        	return TWENTY;
+        } else if (randNum == 3) {
+        	return FIFTY;
         } else {
             return HUNDRED;
         }
